fd7d7d867b18e3fe28234c222541dbcb37e28b8f
Yu Hao Ye <rxye1991@gmail.com>
2019-09-10 22:57:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,16 @@ function sayHiToGrandma(string){
     return "I can\'t hear you!"
   }
 }
+
+function sayHiToGrandma(string){
+  if (string == string.toUpperCase()){
+    return "YES INDEED!"
+  }
+}
+
+function sayHiToGrandma(string){
+  if (string == "I love you, Grandma."){
+    return "I love you, too."
+  }
+}
+
